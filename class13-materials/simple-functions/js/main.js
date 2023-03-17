54a045cc6759d30a1480df585bc3d3780766df6d
Just2Deep <deepakrajsrs@gmail.com>
2023-03-17 19:21:07 +0530

    completed simple funtions

--- class13-materials/simple-functions/js/main.js
+++ class13-materials/simple-functions/js/main.js
@@ -1,5 +1,22 @@
 //create a function that adds two numbers and alerts the sum
 
+function sumTwoNum(a, b){
+    alert(a+b);
+}
+
+sumTwoNum(5, 6);
 //create a function that multiplys three numbers and console logs the product
 
+function multThreeNum(a,b,c) {
+    console.log( a*b*c);
+}
+
+multThreeNum(1,2,5)
+
 //create a function that divides two numbers and returns the ???
+
+function divide(a,b){
+    return a%b;
+}
+
+console.log(divide(10, 3))
