ea1e90914f1cd8206a70edb9d19b2bdddb8633c7
Just2Deep <deepakrajsrs@gmail.com>
2023-03-18 18:17:22 +0530

    assignment 1 done

--- class14-materials/simple-functions/js/main.js
+++ class14-materials/simple-functions/js/main.js
@@ -1,5 +1,23 @@
 //create a function that adds two numbers and alerts the sum
 
+function addNumbers(num1, num2) {
+    const sum = num1 + num2
+    alert(sum)
+}
+
+addNumbers(5, 8)
 //create a function that multiplys three numbers and console logs the product
 
-//create a function that divides two numbers and returns the ???
+function multNumbers(num1, num2 , num3) {
+    const prod = num1* num2 * num3
+    console.log(prod)
+}
+
+multNumbers(1, 2, 5)
+//create a function that divides two numbers and returns the quotient
+ 
+function divides(num1, num2) {
+    return num1 / num2
+}
+const result = divides(4,2)
+console.log(result)
