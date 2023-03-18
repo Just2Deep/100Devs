a99be14d7f3c8ae4376bc6c42f6f30cd230c2848
Just2Deep <deepakrajsrs@gmail.com>
2023-03-18 17:23:39 +0530

    temperature converter soln

--- class14-materials/temperature-converter/js/main.js
+++ class14-materials/temperature-converter/js/main.js
@@ -1,1 +1,19 @@
 //Write your pseduo code first! 
+
+document.querySelector('#sub').addEventListener('click', convert);
+
+// create a function to
+
+function convert() {
+    // get the celcius value
+    const celcius = document.querySelector('#celcius').value
+
+    // convert the temperature using math
+
+    let temp = (celcius * 9 / 5) + 32 ;
+
+    // update the value in website
+
+    document.querySelector('#displayVal').innerText  = temp;
+
+}
