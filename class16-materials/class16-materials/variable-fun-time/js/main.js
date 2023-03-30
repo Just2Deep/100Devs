27acd9f8098c1fd728a5dfcf546f7ff751f6656f
Just2Deep <deepakrajsrs@gmail.com>
2023-03-30 21:27:20 +0530

    class 16 materials

---
+++ class16-materials/class16-materials/variable-fun-time/js/main.js
@@ -0,0 +1,33 @@
+//--- Easy
+//create a variable and assign it a number
+
+let sample_variable = 5;
+
+//minus 10 from that number
+
+sample_variable -= 10;
+
+//print that number to the console
+
+console.log(sample_variable)
+
+//--- Medium
+//create a variable that holds a value from the input
+
+let input_variable = prompt('enter a value')
+
+//add 25 to that number
+
+input_variable += 25
+
+//alert that number
+
+alert(input_variable)
+
+//--- Hard
+//create a variable that holds the h1
+
+let h1_variable = document.getElementsByTagName('h1').value()
+
+//add an event listener to that element that console logs the sum of the two previous variables
+
