edaf760f89f2ea683884c8a1dbadcd5d5f4271e1
Just2Deep <deepakrajsrs@gmail.com>
2023-03-31 21:24:28 +0530

    stop snacking done

--- class16-materials/stop-snacking/js/main.js
+++ class16-materials/stop-snacking/js/main.js
@@ -1,1 +1,10 @@
 //Create a function that grabs the number of snacks from the input and tells you to stop that many times
+
+document.querySelector('#help').addEventListener('click', stopSnacking)
+
+function stopSnacking() {
+    let number = Number(document.querySelector('input').value)
+    for (let i=1; i< number + 1; i++){
+        console.log('STOP')
+    }
+}
