555aa5f5468929b54d8c54de2ae5323f0f113f88
Just2Deep <deepakrajsrs@gmail.com>
2023-03-31 21:18:23 +0530

    sir savage done

--- class16-materials/sir-savage-the-21st/js/main.js
+++ class16-materials/sir-savage-the-21st/js/main.js
@@ -1,2 +1,11 @@
 //Create a function that has a loop that prints '21' 21 times to the console and then call that function
+
+function loop21Times() {
+    for (let i=0; i< 21; i++){
+        document.querySelector('#savageSays').innerText += ' 21'
+        // console.log('21')
+    }
+}
+
+loop21Times()
 //Bonus can you make it print '21' 21 times to the dom?
