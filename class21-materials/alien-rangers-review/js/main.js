9fb86fdde7e198bbe8a05d0d5482a14f8b26183f
Just2Deep <deepakrajsrs@gmail.com>
2023-04-22 17:09:36 +0530

    class 21

--- class21-materials/alien-rangers-review/js/main.js
+++ class21-materials/alien-rangers-review/js/main.js
@@ -2,8 +2,27 @@
 
 //Create and array of tv shows. Loop through and print each show to the console
 
+const tvShows = ['BBT', "Friends", `GOT`]
+
+tvShows.forEach(x => console.log( x ))
+
 //Create and array of numbers
 //Return a new array of numbers that includes every even number from the previous Arrays
+const numbers = [1,2,3,4,5,6,7,8,9,10, 33, -5, -2]
+
+const evenNumbers = numbers.filter(x => x % 2 === 0);
+console.log(evenNumbers)
 
 //Create a function that takes in an array of numbers
 //Alert the sum of the second lowest and the second highest number
+
+function sumOfNumbers(arr) {
+    a = arr.sort( (a,b) => a - b)
+
+    const sum = a[1] + a[a.length - 2]
+    // console.log( a[1] , a[a.length - 2])
+
+    return sum
+}
+
+console.log(sumOfNumbers(numbers))
