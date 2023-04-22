88cdc4eaf0d4edfa9b43aadd361eaa346c053f5b
Just2Deep <deepakrajsrs@gmail.com>
2023-04-22 17:11:59 +0530

    updated numbers as array function

--- class21-materials/alien-rangers-review/js/main.js
+++ class21-materials/alien-rangers-review/js/main.js
@@ -10,8 +10,8 @@ tvShows.forEach(x => console.log( x ))
 //Return a new array of numbers that includes every even number from the previous Arrays
 const numbers = [1,2,3,4,5,6,7,8,9,10, 33, -5, -2]
 
-const evenNumbers = numbers.filter(x => x % 2 === 0);
-console.log(evenNumbers)
+const evenNumbers = nums => nums.filter(x => x % 2 === 0);
+console.log( evenNumbers(numbers) )
 
 //Create a function that takes in an array of numbers
 //Alert the sum of the second lowest and the second highest number
