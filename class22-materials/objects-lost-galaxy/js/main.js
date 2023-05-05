daaf4beab4396b80dca706ac08c8f992081019a2
Just2Deep <deepakrajsrs@gmail.com>
2023-05-05 14:38:55 +0530

    objects day22

--- class22-materials/objects-lost-galaxy/js/main.js
+++ class22-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,20 @@
 //Create a mouse object that has four properties and three methods
+
+const mouse = {}
+
+mouse.color    = 'black';
+mouse.brand    = 'protonic';
+mouse.type     = 'digital';
+mouse.wireless = true;
+
+mouse.leftClick = function() {
+    console.log('LEFT CLICK')
+}
+
+mouse.rightClick = function() {
+    console.log('RIGHT CLICK')
+}
+
+mouse.scroll = function() {
+    console.log('Scrolling')
+}
