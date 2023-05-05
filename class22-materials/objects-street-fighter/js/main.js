94713818613079b5c717078fdf44af83eef8df7f
Just2Deep <deepakrajsrs@gmail.com>
2023-05-05 19:40:56 +0530

    street-fighter class22

--- class22-materials/objects-street-fighter/js/main.js
+++ class22-materials/objects-street-fighter/js/main.js
@@ -1,1 +1,24 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+
+function StreetFighter( name, height, likes, weight ) {
+
+    this.name = name;
+    this.height = height;
+    this.likes  = likes;
+    this.weight = weight;
+
+    this.handMove = function() {
+        console.log('Punch')
+    }
+
+    this.legMove = () => {
+        console.log('Kick')
+    }
+
+    this.specialMove = () => {
+        console.log('Ninja Kick')
+    }
+}
+
+const cammy = new StreetFighter( 'cammy', 5.5, 'cats', 60)
+
