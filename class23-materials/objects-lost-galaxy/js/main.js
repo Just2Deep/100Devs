e8b7ccb36829ddda0795ca1995b553a74d304bc6
Just2Deep <deepakrajsrs@gmail.com>
2023-05-15 21:34:17 +0530

    class 23

--- class23-materials/objects-lost-galaxy/js/main.js
+++ class23-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,17 @@
 //Create a dog object that has four properties and three methods
+
+function Dog( color, breed, name, gender)  {
+    this.color = color,
+    this.breed = breed,
+    this.name  = name,
+    this.gender = gender,
+
+    this.run = () => console.log('run'),
+    this.smell = () => console.log('Sniff, sniff'),
+    this.bark = () => console.log('Woof')
+
+}
+
+const dog = new Dog("black", "Labrodar", "Jackie", "male");
+
+console.log( dog.smell() )
--- class23-materials/objects-tony-hawk-pro-skater/js/main.js
+++ class23-materials/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,15 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+
+function TonyHawkCharacter(characterName, stance, sponsor, characterMove) {
+    this.stance = stance
+    this.characterName  = characterName
+    this.sponsor = sponsor
+    this.specialMove = characterMove
+
+    this.flip = () => console.log( 'Kick Flip ')
+    this.taunt = () => console.log( `Don't make me use my ${this.specialMove}`)
+    this.grab = () => console.log('Melon Grabb')
+
+}
+
+const kareemCampbell = new TonyHawkCharacter( 'Kareem', 'Goofy', 'Element', 'Ghetto Bird')
