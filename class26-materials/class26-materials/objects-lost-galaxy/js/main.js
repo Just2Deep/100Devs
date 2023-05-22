9b8d76e16bbecb6f108451683b07fea3f6084e20
Just2Deep <deepakrajsrs@gmail.com>
2023-05-22 22:25:52 +0530

    class 26 to do items

---
+++ class26-materials/class26-materials/objects-lost-galaxy/js/main.js
@@ -0,0 +1,19 @@
+//Create a pizza object that has four properties and three methods
+
+const pizza = {}
+pizaa.name  = 'Cheese Burst',
+pizaa.size  = 'Large',
+pizaa.price = '$3',
+pizaa.toppings = ['cheese', 'olive', 'onion', 'jalapenos'],
+
+pizza.addBread = function() {
+        console.log('Adding handmade bread')
+    }
+
+pizza.addExtraToppings = function() {
+    console.log('Adding Extra Toppings!!')
+}
+
+pizza.frisbee = function() {
+    console.log('YEEEEEEETTT!!')
+}
