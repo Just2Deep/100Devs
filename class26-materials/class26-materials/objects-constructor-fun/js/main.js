f0498dde33f509c5a5f1c27f68b2898663c94333
Just2Deep <deepakrajsrs@gmail.com>
2023-05-22 22:43:52 +0530

    completed object constructor

--- class26-materials/class26-materials/objects-constructor-fun/js/main.js
+++ class26-materials/class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,22 @@
 //Create a constructor with 4 properties and 3 methods
+
+function Pizza(name, size, price, toppings) { 
+    this.name  = name,
+    this.size  = size,
+    this.price = price,
+    this.toppings = toppings,
+
+    this.addBread = function() {
+            console.log('Adding handmade bread')
+        }
+
+    this.addExtraToppings = function() {
+        console.log('Adding Extra Toppings!!')
+    }
+
+    this.frisbee = function() {
+        console.log('YEEEEEEETTT!!')
+    }
+}
+
+let cheeseBurst = new Pizza('cheese burst', 'medium', '$3.2', ['jalapenos', 'onion', 'pepperoni']);
