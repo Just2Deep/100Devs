ee17ff347a443b2358ffedf2c0fef6ff9a7bb58a
Just2Deep <deepakrajsrs@gmail.com>
2023-05-26 21:31:59 +0530

    updated for NASA images

--- class26-materials/objects-nasa/js/main.js
+++ class26-materials/objects-nasa/js/main.js
@@ -1,1 +1,25 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+const url = 'https://api.nasa.gov/planetary/apod?api_key='
+const API_KEY = '***';
+
+
+document.querySelector('button').addEventListener('click', getNasaImg)
+
+// console.log(document.querySelector('input').value)
+
+function getNasaImg() {
+let mydate= document.querySelector('input').value;
+fetch(`${url}${API_KEY}&date=${mydate}`)
+    .then( res => res.json() )
+    .then( data => {
+        let image = data['url'];
+        let name = data['title'];
+        let description = data['explanation'];
+        console.log(data);
+
+        document.querySelector('img').src = image;
+        document.querySelector('h2').innerText = name;
+        document.querySelector('h3').innerText = description;
+    })
+    .catch( err => console.log(err))
+}
