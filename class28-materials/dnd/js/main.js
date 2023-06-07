0a9c69cb8e5e4cad4fc42f9a1026a1f718bfd6d8
Just2Deep <deepakrajsrs@gmail.com>
2023-06-07 19:53:43 +0530

    completed dnd

--- class28-materials/dnd/js/main.js
+++ class28-materials/dnd/js/main.js
@@ -8,10 +8,21 @@ function getFetch(){
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
+        // console.log(data)
+       document.querySelector('h1').innerHTML = data.index
+       document.querySelector('h2').innerHTML = data.name
+       document.querySelector('h3').innerHTML = data.classes[0].name
+       document.querySelector('ul').innerHTML = ''
        
-      })
+       data.subclasses.forEach(element => {
+        // Create an li
+        const li = document.createElement('li')
+        // Add data into li
+        li.textContent = element.name
+        // Append the li to ul
+        document.querySelector('ul').appendChild(li)
+        })})
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
-
